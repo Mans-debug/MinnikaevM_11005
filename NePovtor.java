7f579ca6f470c0ddedefa46e23743db85e6578ec
MansurMinnikaev <mansurminnikaev@gmail.com>
2020-09-25 21:32:49 +0300

    init

---
+++ NePovtor.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class NePovtor {
+
+    static Scanner scan = new Scanner(System.in);
+    static String slovo = scan.nextLine();
+    static char[] slovoChar = slovo.toCharArray();
+    static int nePovtor = 0;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < slovoChar.length; i++) {
+            int colvo = 0;
+            for (int j = 0; j < slovoChar.length; j++) {
+                if (slovoChar[i] != slovoChar[j]) {
+                    colvo++;
+                }
+            }
+            if (colvo == slovoChar.length - 1) {
+                nePovtor++;
+            }
+
+        }
+        System.out.println(nePovtor);
+    }
+
+}
+
+
+
